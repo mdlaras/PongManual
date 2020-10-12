5228cd4d250701ab6ed5c6442fd931baed6e92a9
mdlaras <52792426+mdlaras@users.noreply.github.com>
2020-10-12 09:58:01 +0700

    code review 1 ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -10,24 +10,33 @@ public class Ball : MonoBehaviour
     Vector3 point1;
     Vector3 point2;
     float mag;
-    float lastx = 0;
-    float lasty = 0;
     float posx;
     public float posy;
     int collidedOn = 5;
-    float playerpos;
-    float enemypos;
-    PaddlePlayer player;
-    PaddleEnemy enemy;
+    float offset = 3f;
+    float ballVelocity;
+    [SerializeField] PaddlePlayer player;
+    [SerializeField] PaddleEnemy enemy;
+    [SerializeField] Rigidbody2D WallTop;
+    [SerializeField] Rigidbody2D WallBottom;
+    [SerializeField] Rigidbody2D WallRight;
+    [SerializeField] Rigidbody2D WallLeft;
 
+    void Collide()
+    {
+        Debug.Log(collidedOn);
+        point2 = transform.position;
+        mag = Vector3.Distance(point2, point1);
+        unitvec = (point2 - point1) / mag;
+        Debug.Log(unitvec);
+        point1 = point2;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        ballVelocity = 5f;
         transform.position = new Vector3(0.03f, 0.03f, 0);
         point1 = transform.position;
-        player = FindObjectOfType<PaddlePlayer>();
-        enemy = FindObjectOfType<PaddleEnemy>();
-        playerpos = player.transform.position.y;
     }
 
     // Update is called once per frame
@@ -35,98 +44,47 @@ public class Ball : MonoBehaviour
     {
         posx = transform.position.x;
         posy = transform.position.y;
-        playerpos = player.transform.position.y;
-        enemypos = enemy.transform.position.y;
-        if (posy <= -5.03f)
+        float playertopbound = player.transform.position.y + offset;
+        float playerlowbound = player.transform.position.y - offset;
+        float enemytopbound = enemy.transform.position.y + offset;
+        float enemylowbound = enemy.transform.position.y - offset;
+        if (posy <= WallBottom.transform.position.y)
         { 
             collidedOn = 1;
-            Debug.Log(collidedOn);
-            point2 = transform.position;
-            mag = Vector3.Distance(point2, point1);
-            unitvec = (point2 - point1);
-            Vector3.Normalize(unitvec);
-            Debug.Log(unitvec);
-            lastx = transform.position.x;
-            lasty = transform.position.y;
-            point1 = point2;
+            Collide();
         }
-        if (posy >= 4.96f)
+        if (posy >= WallTop.transform.position.y)
         {
-            
             collidedOn = 2;
-            Debug.Log(collidedOn);
-            point2 = transform.position;
-            mag = Vector3.Distance(point2, point1);
-            unitvec = (point2 - point1);
-            Vector3.Normalize(unitvec);
-            Debug.Log(unitvec);
-            lastx = transform.position.x;
-            lasty = transform.position.y;
-            point1 = point2;
+            Collide();
         }
-        if ((posx <= -8.87) || ((posx <= -6.96) && ((posy <= playerpos + 3f) && (posy >= playerpos - 3f))))
+        if ((posx <= WallLeft.transform.position.x) || ((posx <= player.transform.position.x) && ((posy <=playertopbound) && (posy >= playerlowbound))))
         {
-            
             collidedOn = 3;
-            Debug.Log(collidedOn);
-            point2 = transform.position;
-            mag = Vector3.Distance(point2, point1);
-            unitvec = (point2 - point1);
-            Vector3.Normalize(unitvec);
-            Debug.Log(unitvec);
-            lastx = transform.position.x;
-            lasty = transform.position.y;
-            point1 = point2;
+            Collide();
         }
-        if ((posx >= 8.96) || ((posx >= 7.12) && ((posy <= enemypos + 3f) && (posy >= enemypos - 3f))))
+        if ((posx >= WallRight.transform.position.x) || ((posx >= enemy.transform.position.x) && ((posy <= enemytopbound) && (posy >=enemylowbound))))
         {
-           
             collidedOn = 4;
-            Debug.Log(collidedOn);
-            point2 = transform.position;
-            mag = Vector3.Distance(point2, point1);
-            unitvec = (point2 - point1);
-            Vector3.Normalize(unitvec);
-            Debug.Log(unitvec);
-            lastx = transform.position.x;
-            lasty = transform.position.y;
-            point1 = point2;
+            Collide();
         };
-        
-        if (unitvec.Equals(new Vector3(0, 0, 0)))
-        {
-            
-            unitvec = new Vector3(20f, 20f, 0);
-            if (collidedOn == 3)
-            {
-                unitvec = new Vector3(-20f, 20f, 0);
-            }
-            else if (collidedOn == 1)
-            {
-                unitvec = new Vector3(20f, -40f, 0);
-            }
-            else if (collidedOn == 2){
-                unitvec = new Vector3(20f, 40f, 0);
-            }
-            Vector3.Normalize(unitvec);
 
-        }
         switch (collidedOn)
         {
             case 1:
-                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(0f,1f,0f)) * 0.003f;
+                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(0f,1f,0f)) * ballVelocity * Time.deltaTime;
                 break;
             case 2:
-                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(0f, -1f, 0f)) * 0.003f;
+                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(0f, -1f, 0f)) * ballVelocity * Time.deltaTime;
                 break;
             case 3:
-                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(-1f, 0f, 0f)) * 0.003f;
+                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(-1f, 0f, 0f)) * ballVelocity * Time.deltaTime;
                 break; 
             case 4:
-                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(1f, 0f, 0f)) * 0.003f;
+                transform.position = transform.position + Vector3.Reflect(unitvec, new Vector3(1f, 0f, 0f)) * ballVelocity * Time.deltaTime;
                 break;
             case 5:
-                transform.position = transform.position - new Vector3(0.03f, 0.03f, 0);
+                transform.position = transform.position - new Vector3(1f, 1f, 0) * ballVelocity * Time.deltaTime;
                 break;
         }
 
